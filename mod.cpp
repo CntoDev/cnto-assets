f72a0076a47da5e1bf23308cfb484f18711fcc90
freghar <freghar@dummy.tld>
2016-06-12 18:57:59 +0200

    add CNTO logo
    Signed-off-by: freghar <freghar@dummy.tld>

--- mod.cpp
+++ mod.cpp
@@ -2,3 +2,5 @@ name = "CNTO Assets";
 author = "CNTO Design Team";
 hideName = 0;
 overview = "Custom CNTO-produced gear (uniform, vests, etc.), flags, artwork, textures, models, ...";
+picture = "cnto_logo.paa";
+logo = "cnto_logo.paa";
