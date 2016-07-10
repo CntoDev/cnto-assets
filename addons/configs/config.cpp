3afb86524b025fbb608f794325a403b84f00ed2b
freghar <freghar@dummy.tld>
2016-07-10 15:59:39 +0200

    don't override hiddenSelections with {"camo"}
    Some mods don't define the main camo as "camo", but ie. "Camo1".
    
    Signed-off-by: freghar <freghar@dummy.tld>

--- addons/configs/config.cpp
+++ addons/configs/config.cpp
@@ -10,7 +10,6 @@ class CfgPatches {
     class name : base { \
         scope = 1; \
         uniformClass = name; \
-        hiddenSelections[] += {"camo"}; \
         hiddenSelectionsTextures[] = { paa }; \
         model = p3d; \
     }
@@ -18,7 +17,6 @@ class CfgPatches {
     class name : base { \
         scope = 2; \
         displayName = text; \
-        hiddenSelections[] += {"camo"}; \
         hiddenSelectionsTextures[] = { paa }; \
         class ItemInfo : ItemInfo { \
             uniformClass = name; \
@@ -29,7 +27,6 @@ class CfgPatches {
     class name : base { \
         scope = 2; \
         displayName = text; \
-        hiddenSelections[] += {"camo"}; \
         hiddenSelectionsTextures[] = { paa }; \
     }
 
@@ -37,22 +34,14 @@ class CfgPatches {
     class name : base { \
         scope = 2; \
         displayName = text; \
-        hiddenSelections[] += {"camo"}; \
         hiddenSelectionsTextures[] = { paa }; \
-        class ItemInfo : ItemInfo { \
-            hiddenSelections[] += {"camo"}; \
-        }; \
     }
 
 #define HEADGEAR(name, base, text, paa) \
     class name : base { \
         scope = 2; \
         displayName = text; \
-        hiddenSelections[] += {"camo"}; \
         hiddenSelectionsTextures[] = { paa }; \
-        class ItemInfo : ItemInfo { \
-            hiddenSelections[] += {"camo"}; \
-        }; \
     }
 
 class CfgVehicles {
@@ -133,49 +122,48 @@ class CfgWeapons {
 
     /* ------------------------------------------- */
 
-    class Vest_NoCamo_Base;
-    class V_PlateCarrier1_rgr : Vest_NoCamo_Base { class ItemInfo; };
-    class V_PlateCarrier2_rgr : V_PlateCarrier1_rgr { class ItemInfo; };
-    class V_PlateCarrierGL_rgr : Vest_NoCamo_Base { class ItemInfo; };
-    class V_PlateCarrierSpec_rgr : Vest_NoCamo_Base { class ItemInfo; };
+    /* V_PlateCarrier1_rgr / V_PlateCarrier2_rgr have
+     * hiddenSelections = {}, no {"camo"}, so use _blk */
+    class V_PlateCarrier1_blk;
+    class V_PlateCarrier2_blk;
+    class V_PlateCarrierGL_rgr;
+    class V_PlateCarrierSpec_rgr;
 
     /*
      * Flecktarn Vests
      */
 
     #define TXR(path) \cnto\assets\flecktarn_vests\##path
-    VEST(cnto_flecktarn_v_l_desert, V_PlateCarrier1_rgr, "CNTO Flecktarn Carrier Lite (Desert)", TXR(desert.paa));
-    VEST(cnto_flecktarn_v_desert, V_PlateCarrier2_rgr, "CNTO Flecktarn Carrier (Desert)", TXR(desert.paa));
+    VEST(cnto_flecktarn_v_l_desert, V_PlateCarrier1_blk, "CNTO Flecktarn Carrier Lite (Desert)", TXR(desert.paa));
+    VEST(cnto_flecktarn_v_desert, V_PlateCarrier2_blk, "CNTO Flecktarn Carrier (Desert)", TXR(desert.paa));
     VEST(cnto_flecktarn_v_h_desert, V_PlateCarrierGL_rgr, "CNTO Flecktarn Carrier Heavy (Desert)", TXR(desert_heavy.paa));
     VEST(cnto_flecktarn_v_s_desert, V_PlateCarrierSpec_rgr, "CNTO Flecktarn Carrier Special (Desert)", TXR(desert_heavy.paa));
 
-    VEST(cnto_flecktarn_v_l_forest, V_PlateCarrier1_rgr, "CNTO Flecktarn Carrier Lite (Forest)", TXR(forest.paa));
-    VEST(cnto_flecktarn_v_forest, V_PlateCarrier2_rgr, "CNTO Flecktarn Carrier (Forest)", TXR(forest.paa));
+    VEST(cnto_flecktarn_v_l_forest, V_PlateCarrier1_blk, "CNTO Flecktarn Carrier Lite (Forest)", TXR(forest.paa));
+    VEST(cnto_flecktarn_v_forest, V_PlateCarrier2_blk, "CNTO Flecktarn Carrier (Forest)", TXR(forest.paa));
     VEST(cnto_flecktarn_v_h_forest, V_PlateCarrierGL_rgr, "CNTO Flecktarn Carrier Heavy (Forest)", TXR(forest_heavy.paa));
     VEST(cnto_flecktarn_v_s_forest, V_PlateCarrierSpec_rgr, "CNTO Flecktarn Carrier Special (Forest)", TXR(forest_heavy.paa));
 
-    VEST(cnto_flecktarn_v_l_jungle, V_PlateCarrier1_rgr, "CNTO Flecktarn Carrier Lite (Jungle)", TXR(jungle.paa));
-    VEST(cnto_flecktarn_v_jungle, V_PlateCarrier2_rgr, "CNTO Flecktarn Carrier (Jungle)", TXR(jungle.paa));
+    VEST(cnto_flecktarn_v_l_jungle, V_PlateCarrier1_blk, "CNTO Flecktarn Carrier Lite (Jungle)", TXR(jungle.paa));
+    VEST(cnto_flecktarn_v_jungle, V_PlateCarrier2_blk, "CNTO Flecktarn Carrier (Jungle)", TXR(jungle.paa));
     VEST(cnto_flecktarn_v_h_jungle, V_PlateCarrierGL_rgr, "CNTO Flecktarn Carrier Heavy (Jungle)", TXR(jungle_heavy.paa));
     VEST(cnto_flecktarn_v_s_jungle, V_PlateCarrierSpec_rgr, "CNTO Flecktarn Carrier Special (Jungle)", TXR(jungle_heavy.paa));
 
-    VEST(cnto_flecktarn_v_l_mediterranean, V_PlateCarrier1_rgr, "CNTO Flecktarn Carrier Lite (Mediterranean)", TXR(mediterranean.paa));
-    VEST(cnto_flecktarn_v_mediterranean, V_PlateCarrier2_rgr, "CNTO Flecktarn Carrier (Mediterranean)", TXR(mediterranean.paa));
+    VEST(cnto_flecktarn_v_l_mediterranean, V_PlateCarrier1_blk, "CNTO Flecktarn Carrier Lite (Mediterranean)", TXR(mediterranean.paa));
+    VEST(cnto_flecktarn_v_mediterranean, V_PlateCarrier2_blk, "CNTO Flecktarn Carrier (Mediterranean)", TXR(mediterranean.paa));
     VEST(cnto_flecktarn_v_h_mediterranean, V_PlateCarrierGL_rgr, "CNTO Flecktarn Carrier Heavy (Mediterranean)", TXR(mediterranean_heavy.paa));
     VEST(cnto_flecktarn_v_s_mediterranean, V_PlateCarrierSpec_rgr, "CNTO Flecktarn Carrier Special (Mediterranean)", TXR(mediterranean_heavy.paa));
 
-    VEST(cnto_flecktarn_v_l_snow, V_PlateCarrier1_rgr, "CNTO Flecktarn Carrier Lite (Snow)", TXR(snow.paa));
-    VEST(cnto_flecktarn_v_snow, V_PlateCarrier2_rgr, "CNTO Flecktarn Carrier (Snow)", TXR(snow.paa));
+    VEST(cnto_flecktarn_v_l_snow, V_PlateCarrier1_blk, "CNTO Flecktarn Carrier Lite (Snow)", TXR(snow.paa));
+    VEST(cnto_flecktarn_v_snow, V_PlateCarrier2_blk, "CNTO Flecktarn Carrier (Snow)", TXR(snow.paa));
     VEST(cnto_flecktarn_v_h_snow, V_PlateCarrierGL_rgr, "CNTO Flecktarn Carrier Heavy (Snow)", TXR(snow_heavy.paa));
     VEST(cnto_flecktarn_v_s_snow, V_PlateCarrierSpec_rgr, "CNTO Flecktarn Carrier Special (Snow)", TXR(snow_heavy.paa));
 
     /* ------------------------------------------- */
 
-    class ItemCore;
-    class HelmetBase;
-    class H_HelmetB : ItemCore { class ItemInfo; };
-    class H_Booniehat_khk : HelmetBase { class ItemInfo; };
-    class H_Beret_blk : HelmetBase { class ItemInfo; };
+    class H_HelmetB;
+    class H_Booniehat_khk;
+    class H_Beret_blk;
 
     /*
      * Flecktarn Headgear
